0582b8f55c472680a7a6e471e6c9b6e673e5ab3b
Neim-Ramazanoglu <mr.ramazanoglu@gmail.com>
2021-07-28 15:51:18 +0300

    json added

--- src/components/Netflix/Netflix-logo.js
+++ src/components/Netflix/Netflix-logo.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './netflix.css'
+import React from "react";
+import "./netflix.css";
 
 function Logo() {
-    return <img className="logo" src="./images/netflix-logo.png" ></img>
+  return (
+    <div>
+      <div className="logo1">
+        <img className="logo" src="./images/netflix-logo.png"></img>
+      </div>
+      <div className="navbar1">
+        <span className="navbar">Ana sayfa</span>
+        <span className="navbar">Diziler</span>
+      </div>
+    </div>
+  );
 }
-
-
-
-
 
 export default Logo;
